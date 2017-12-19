1477053256acc6814a85865a1a99576786fd8e8c
vitoralvesdev <vitorhugoalvees@gmail.com>
2017-12-19 13:00:12 -0200

    .

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -71,7 +71,7 @@ const HomeScreenRouter = DrawerNavigator(
     }
   },
   {
-    initialRouteName: 'PontosTuristicos',
+    initialRouteName: 'Inicio',
     contentComponent: props => <SideMenu {...props} />,
     drawerBackgroundColor: 'transparent',
     contentOptions : {
